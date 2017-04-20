e235b3ec20eddf8fdd1d5345360dadc8b470e59d
Louis Caudevilla <louis.caudevilla@ffshmbp-0054.local>
2017-04-20 11:34:50 +0800

    adding ChunkyMonkey Exercise

---
+++ front-end/basicAlgorithmScripting/ChunkyMonkey.js
@@ -0,0 +1,19 @@
+//Write a function that splits an array (first argument) into groups the length of size
+//(second argument) and returns them as a two-dimensional array.
+//Here are some helpful links:
+//Array.prototype.push()
+//Array.prototype.slice()
+//chunkArrayInGroups(["a", "b", "c", "d"], 2) should return [["a", "b"], ["c", "d"]].
+//chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3) should return [[0, 1, 2], [3, 4, 5]].
+//chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2) should return [[0, 1], [2, 3], [4, 5]].
+//chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4) should return [[0, 1, 2, 3], [4, 5]].
+//chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3) should return [[0, 1, 2], [3, 4, 5], [6]].
+//chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4) should return [[0, 1, 2, 3], [4, 5, 6, 7], [8]].
+//chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2) should return [[0, 1], [2, 3], [4, 5], [6, 7], [8]].
+
+function chunkArrayInGroups(arr, size) {
+  // Break it up.
+  console.log(arr);
+}
+
+chunkArrayInGroups(["a", "b", "c", "d"], 2);
