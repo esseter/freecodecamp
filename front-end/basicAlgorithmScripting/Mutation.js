c99664be15903be79e078846a0caa50f6cabf159
Louis Caudevilla <louis.caudevilla@ffshmbp-0054.local>
2017-04-21 20:50:00 +0800

    Mutation exercises finished

--- front-end/basicAlgorithmScripting/Mutation.js
+++ front-end/basicAlgorithmScripting/Mutation.js
@@ -2,7 +2,8 @@
 //contains all of the letters of the string in the second element of the array.
 
 //For example, ["hello", "Hello"], should return true
-//because all of the letters in the second string are present in the first, ignoring case.
+//because all of the letters in the second string are present in the first,
+// ignoring case.
 
 //The arguments ["hello", "hey"] should return false
 //because the string "hello" does not contain a "y".
@@ -10,9 +11,9 @@
 //Lastly, ["Alien", "line"], should return true
 //because all of the letters in "line" are present in "Alien".
 
-Here are some helpful links:
+//Here are some helpful links:
 
-String.prototype.indexOf()
+// String.prototype.indexOf()
 
 //mutation(["hello", "hey"]) should return false.
 //mutation(["hello", "Hello"]) should return true.
@@ -23,3 +24,13 @@ String.prototype.indexOf()
 //mutation(["floor", "for"]) should return true.
 //mutation(["hello", "neo"]) should return false.
 //mutation(["voodoo", "no"]) should return false.
+
+function mutation(arr) {
+ var test = arr[1].toLowerCase();
+ var target = arr[0].toLowerCase();
+ for (i=0;i<test.length;i++) {
+   if (target.indexOf(test[i]) < 0)
+     return false;
+ }
+ return true;
+}
