063fc15bc0290482e2f4f5aefad3972c9fec65c2
Caudevilla Louis <caudevilla@MacBook-Pro-de-Caudevilla.local>
2017-04-22 15:38:13 +0800

    finished challenge ceasar and basic scripting

--- front-end/basicAlgorithmScripting/CaesarsCipher.js
+++ front-end/basicAlgorithmScripting/CaesarsCipher.js
@@ -19,9 +19,41 @@ Write a function which takes a ROT13 encoded string as input and returns a decod
 //rot13("GUR DHVPX OEBJA QBT WHZCRQ BIRE GUR YNML SBK.") should decode to "THE QUICK BROWN DOG JUMPED OVER THE LAZY FOX."
 
 
-function rot13(str) { // LBH QVQ VG!
 
-  return str;
+function rot13(str) { // LBH QVQ VG!
+  // transform the string in charcode
+  var codeStr = []; // the serie of code will be here
+  var code = [];
+  for (i = 0; i < str.length; i++){
+    code = str.charCodeAt(i);
+    codeStr.push(code);
+  }
+  //Decode the code for each letter
+  for (i = 0; i < codeStr.length; i++) {
+
+    //N - Z and n - z decode
+
+    if ((codeStr[i] >= 78 && codeStr[i] <= 90) || (codeStr[i] >= 110 && codeStr[i] <= 122)){
+      codeStr[i] = codeStr[i] -13;
+    }
+
+    // A-M and a-m decode
+
+    else if((codeStr[i] >= 65 && codeStr[i] <= 77) || (codeStr[i] >= 97 && codeStr[i] <= 109)) {
+      codeStr[i] = codeStr[i] + 13;
+    }
+  }
+
+
+
+  // this is last step to go from charcode to string
+  var solution = [];
+  for (j=0; j < codeStr.length; j++){
+  var letter = String.fromCharCode(codeStr[j]);
+  solution.push(letter);
+  }
+
+  return solution.join('');
 }
 
 // Change the inputs below to test
