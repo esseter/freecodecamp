5822b00d3dff1b1c7c28ffba3622f56ba070de9f
Louis Caudevilla <louis.caudevilla@ffshmbp-0054.local>
2017-04-25 17:56:22 +0800

    quote system done, design process on going, twitter left to make

--- front-end/Intermediate Front-end  Development Projects/Random Quote Machine/js/javascript.js
+++ front-end/Intermediate Front-end  Development Projects/Random Quote Machine/js/javascript.js
@@ -1,19 +1,15 @@
-$document.ready( function() {
+$(document).ready(function(){
 
-  $("#getMessage").click(function(){
-
-    $.getJSON("#"),function(json){
-      var html = "";
-
-      json.forEach(function(val) {
-        var keys = Object.key(val);
-        html += "<div class='quoter'>";
-        html += "<p>" + val.quoter + "</p>";
-        html += "</div>";
-      })；
-
-      $.(".quote").html(html);
-
-    }；
-  })；
-})；
+  $('#getQuote').on('click', function(e) {
+      e.preventDefault();
+      $.ajax( {
+        url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
+        success: function(data) {
+          var post = data.shift(); // The data is an array of posts. Grab the first one.
+          $('.quoter').text(post.title);
+          $('.quote').html('<p>" ' + post.content + '  "</p>');
+        },
+        cache: false
+      });
+    });
+  });
