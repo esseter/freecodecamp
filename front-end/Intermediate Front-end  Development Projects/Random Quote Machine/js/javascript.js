98cf094f4b6b3a3bcee9a5ad1cfc233573def9ca
Louis Caudevilla <louis.caudevilla@ffshmbp-0054.local>
2017-04-25 18:40:13 +0800

    changing color palette

--- front-end/Intermediate Front-end  Development Projects/Random Quote Machine/js/javascript.js
+++ front-end/Intermediate Front-end  Development Projects/Random Quote Machine/js/javascript.js
@@ -6,8 +6,8 @@ $(document).ready(function(){
         url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
         success: function(data) {
           var post = data.shift(); // The data is an array of posts. Grab the first one.
-          $('.quoter').text(post.title);
-          $('.quote').html('<p>" ' + post.content + '  "</p>');
+          $('.quoter').html("<p><strong>" + post.title + "</strong></p>");
+          $('.quote').html('<p><em> ' + post.content + '<em></p>');
         },
         cache: false
       });
