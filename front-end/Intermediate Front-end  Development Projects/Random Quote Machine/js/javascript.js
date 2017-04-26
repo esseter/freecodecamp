4284b1cbd38d9c02f73ce1b959dd7b47f77e4654
Louis Caudevilla <louis.caudevilla@ffshmbp-0054.local>
2017-04-26 19:20:57 +0800

    twitter script

--- front-end/Intermediate Front-end  Development Projects/Random Quote Machine/js/javascript.js
+++ front-end/Intermediate Front-end  Development Projects/Random Quote Machine/js/javascript.js
@@ -5,11 +5,26 @@ $(document).ready(function(){
       $.ajax( {
         url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
         success: function(data) {
-          var post = data.shift(); // The data is an array of posts. Grab the first one.
+          var post = data.shift();
+          var tweeterMessage = post.content;
+
           $('.quoter').html("<p><strong>" + post.title + "</strong></p>");
           $('.quote').html('<p><em> ' + post.content + '<em></p>');
+          return tweeterMessage;
         },
         cache: false
       });
     });
   });
+
+/*
+  function twitterSplit(test) {
+    test = test.split("");
+    var correctTest = [];
+    for (var i = 3; i < test.length - 6; i++) {
+      correctTest.push(test[i]);
+    }
+    return correctTest;
+  }
+twitterSplit("<p>Design is thinking made visual.  <\/p>\n");
+ */
