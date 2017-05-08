5899ae78ac06d151d8ed7b268c02f221caf6331d
Louis Caudevilla <louis.caudevilla@ffshmbp-0054.local>
2017-05-08 18:16:15 +0800

    succeed calling the wikipedia api
    Trying to find a way to display results in a proper way

--- front-end/Intermediate Front-end  Development Projects/WikipediaViewer/js/javascript.js
+++ front-end/Intermediate Front-end  Development Projects/WikipediaViewer/js/javascript.js
@@ -1,12 +1,25 @@
 $(document).ready(function() {
 
-  var searchWord = '';
+  var keyword = 'louis';
 
+  // Using XMLHttpRequest
 
-  $.ajax({
-    url: 'https://en.wikipedia.org/w/api.php?action=query&meta=siteinfo&titles=louis&format=json&callback=\?',
-    success: function(data) {
-      console.log(data);
-    }
-  });
+    $.ajax({
+      url: "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=Louis&prop=info&inprop=url&utf8=&format=json",
+
+     dataType: "jsonp",
+     success: function(response) {
+         console.log(response.query);
+         if (response.query.searchinfo.totalhits === 0) {
+           showError(keyword);
+         }
+         else {
+           console.log(response);
+         }
+    },
+     error: function () {
+      alert("Error retrieving search results, please refresh the page");
+     }
+
+   });
 });
