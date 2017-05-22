35446aefcea5f4612ff618c8a812f5b8a8dec3fa
Louis Caudevilla <louis.caudevilla@ffshmbp-0054.local>
2017-05-22 13:52:54 +0800

    js powered and working to call offline

--- front-end/Intermediate Front-end  Development Projects/Twitch/_site/js/javascript.js
+++ front-end/Intermediate Front-end  Development Projects/Twitch/_site/js/javascript.js
@@ -7,19 +7,32 @@ $(document).ready(function() {
     var urlChannel = 'https://wind-bow.glitch.me/twitch-api/channels/' + users[i];
     var urlBio = 'https://wind-bow.glitch.me/twitch-api/users/' + users[i];
 
+
+
     $.getJSON(urlStream, function(data){
 
-
       if (data.stream === null) {
-        $.getJSON(urlBio, function(dataBis) {
 
-          var gameOff = dataBis.bio;
+        var links = data._links.channel;
+
+        function makeLinksGreatAgain(links) {
+          links = links.split("/");
+          var lastSpots = links[links.length - 1];
+          var realLinks = 'https://wind-bow.glitch.me/twitch-api/channels/' + lastSpots;
+          return realLinks;
+        };
+
+        console.log(makeLinksGreatAgain(links));
+
+
+        $.getJSON(makeLinksGreatAgain(data._links.channel), function(dataBis) {
+
+          var gameOff = dataBis.game;
           var nameOff = dataBis.display_name;
           var logoOff = dataBis.logo;
-          var htmlOff = '<div class="streamer"><div class="row"><div class="col-xs-3"><img src="'+ logoOff +'" alt="'+ nameOff + '" class="logo"></div><div class="stream"><div class="col-xs-6"><h4 class="streamerName">' + nameOff + '</h4><p class="activity">'+ gameOff +'</p></div></div><div class="streamStatus"><div class="col-xs-3"><i class="fa fa-toggle-off" aria-hidden="true"></i></div></div></div></div>';
+          var htmlOff = '<div class="streamer"><div class="row"><div class="col-xs-3"><img src="'+ logoOff +'" alt="'+ nameOff + '" class="logo"></div><div class="stream"><div class="col-xs-6"><h4 class="streamerName">' + nameOff + '</h4><p class="activity">Usually playing: '+ gameOff +'</p></div></div><div class="streamStatus"><div class="col-xs-3"><i class="fa fa-toggle-off" aria-hidden="true"></i></div></div></div></div>';
           $('#all').append(htmlOff);
           $('#offline').append(htmlOff);
-          console.log(dataBis);
         });
       }
       else {
