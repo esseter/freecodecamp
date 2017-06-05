7272da9186e72698e37338c8b3db7e6595d60db7
Louis Caudevilla <louis.caudevilla@ffshmbp-0054.local>
2017-06-05 19:01:45 +0800

    setup

--- front-end/IntermediateAlgorithmScripting/WhereforeArtThou.js
+++ front-end/IntermediateAlgorithmScripting/WhereforeArtThou.js
@@ -31,10 +31,11 @@ function whatIsInAName(collection, source) {
   var arr = [];
   // Only change code below this line
 
-
+  // Keys of source
+  var sourceKeys = Object.keys(source);
   // Only change code above this line
-  return arr;
+    return obj.hasOwnProperty(sourceKeys);
 }
 
-whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null },
-{ first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+whatIsInAName([{ "a": 1, "b": 2 }, { "a": 1 }, { "a": 1, "b": 2, "c": 2 }],
+{ "a": 1, "c": 2 })
