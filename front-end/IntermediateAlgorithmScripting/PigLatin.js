33c71c71bf8b0c9842ead84f09cf79bf828f9fd8
Louis Caudevilla <louis.caudevilla@ffshmbp-0054.local>
2017-06-09 11:00:39 +0800

    Adding Pig latin exercice

---
+++ front-end/IntermediateAlgorithmScripting/PigLatin.js
@@ -0,0 +1,31 @@
+/* Translate the provided string to pig latin.
+
+Pig Latin takes the first consonant (or consonant cluster) of an English word,
+ moves it to the end of the word and suffixes an "ay".
+
+If a word begins with a vowel you just add "way" to the end.
+
+Input strings are guaranteed to be English words in all lowercase.
+
+
+Here are some helpful links:
+
+Array.prototype.indexOf()
+Array.prototype.push()
+Array.prototype.join()
+String.prototype.substr()
+String.prototype.split()
+
+translatePigLatin("california") should return "aliforniacay".
+translatePigLatin("paragraphs") should return "aragraphspay".
+translatePigLatin("glove") should return "oveglay".
+translatePigLatin("algorithm") should return "algorithmway".
+translatePigLatin("eight") should return "eightway".
+*/
+
+
+function translatePigLatin(str) {
+  return str;
+}
+
+translatePigLatin("consonant");
