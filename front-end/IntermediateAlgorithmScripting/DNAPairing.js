c40b907cdbb4e3b475b1a6e08cb7533c553c253b
Caudevilla Louis <caudevilla@MacBook-Pro-de-Caudevilla.local>
2017-06-10 15:59:53 +0800

    Adding the switch

--- front-end/IntermediateAlgorithmScripting/DNAPairing.js
+++ front-end/IntermediateAlgorithmScripting/DNAPairing.js
@@ -19,8 +19,27 @@ pairElement("ATCGA") should return [["A","T"],["T","A"],["C","G"],["G","C"],["A"
 pairElement("TTGAG") should return [["T","A"],["T","A"],["G","C"],
 */
 
+
 function pairElement(str) {
-  return str;
+  var strClean = [];
+  str = str.split("");
+  for(i = 0 ; i < str.length ; i++){
+    var strSplit = str.splice(i,1);
+
+    switch (strSplit[0]) {
+      case 'G':
+        strSplit.push('B');
+        strClean.push(strSplit);
+        break;
+      case 'C':
+        strSplit.push('A');
+        strClean.push(strSplit);
+        break;
+      default:
+        console.log('this is not a DNA sequence');
+    }
+  }
+  return strClean;
 }
 
 pairElement("GCG");
