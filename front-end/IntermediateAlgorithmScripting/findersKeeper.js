2dfe353ab7221d679ac73c4f7ecf768941afcd01
Louis Caudevilla <louis.caudevilla@ffshmbp-0054.local>
2017-06-14 17:34:00 +0800

    make first takers

--- front-end/IntermediateAlgorithmScripting/findersKeeper.js
+++ front-end/IntermediateAlgorithmScripting/findersKeeper.js
@@ -15,8 +15,14 @@
 
 
 function findElement(arr, func) {
-  var num = 0;
-  return num;
+  var num = arr;
+  for (var i = 0; i < arr.length; i++) {
+    if (func(arr[i]) === true) {
+      return arr[i];
+      break;
+    }
+  }
+  return undefined;
 }
 
-findElement([1, 2, 3, 4], function(num){ return num % 2 === 0; });
+findElement([1, 3, 5, 9], function(num){ return num % 2 === 0; });
