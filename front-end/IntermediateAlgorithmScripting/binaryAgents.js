5e94b3689aa84a96f5fd6b72186da685d7d05cd3
Louis Caudevilla <louis.caudevilla@ffshmbp-0054.local>
2017-06-16 10:10:42 +0800

    Finish binaryAgents

--- front-end/IntermediateAlgorithmScripting/binaryAgents.js
+++ front-end/IntermediateAlgorithmScripting/binaryAgents.js
@@ -17,7 +17,22 @@
 
 
 function binaryAgent(str) {
-  return str;
+  var binaryConverter = function(num) {
+    return parseInt(num,2);
+  };
+  str = str.split(" ");
+
+  for (var i = 0; i < str.length; i++) {
+    if (str[i].length === 8) {
+      str[i] = String.fromCharCode(binaryConverter(str[i]));
+
+    }
+    else {
+      console.log("This is not a binary number for charcode" + str[i]);
+    }
+  }
+
+  return str.join("");
 }
 
 binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
