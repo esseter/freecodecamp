280146acca4b2ae6ed460d10beab381d6c106871
Louis Caudevilla <louis.caudevilla@ffshmbp-0054.local>
2017-06-26 11:34:45 +0800

    Finish the new inventory script

--- front-end/AdvanceAlgorithmScripting/InventoryUpdate.js
+++ front-end/AdvanceAlgorithmScripting/InventoryUpdate.js
@@ -25,6 +25,45 @@ updateInventory([[0, "Bowling Ball"], [0, "Dirty Sock"], [0, "Hair Pin"], [0, "M
 
 function updateInventory(arr1, arr2) {
     // All inventory must be accounted for or you're fired!
+    function isItThere(arrx, arry) {
+      if (arrx[1] === arry[1]) {
+        arrx[0] += arry[0];
+        console.log('j ai ajoute ' + arry);
+        return true;
+      }
+    };
+    // Here we push the items that are already in the list and removing them from the new inventory object
+    for (var i = 0; i < arr1.length; i++) {
+      for (var j = 0; j < arr2.length; j++) {
+        if (isItThere(arr1[i], arr2[j]) === true) {
+          arr2.splice(j,1);
+          console.log('J ai coupe ' + j);
+        }
+      }
+    }
+    // Now we push the other items that are not already in the current inventory
+    var x = 0;
+    while (x < arr2.length) {
+      if (arr1.indexOf(arr2[x][1]) == -1) {
+        arr1.push(arr2[x]);
+      }
+      x++;
+    }
+
+    //we change the order of the individual arrays within the object to make the sort option automatic
+    var y = 0;
+    while (y < arr1.length) {
+      arr1[y].reverse();
+      y++;
+    }
+
+    arr1 = arr1.sort();
+    // then we put that to the original order for the individual arrays within the object
+    var z = 0;
+    while (z < arr1.length) {
+      arr1[z].reverse();
+      z++;
+    }
     return arr1;
 }
 
@@ -35,7 +74,6 @@ var curInv = [
     [1, "Hair Pin"],
     [5, "Microphone"]
 ];
-
 var newInv = [
     [2, "Hair Pin"],
     [3, "Half-Eaten Apple"],
