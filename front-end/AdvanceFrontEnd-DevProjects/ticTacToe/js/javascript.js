3d8ed0248f17dff0bf764c78315c291b0c3920cd
Caudevilla Louis <caudevilla@MacBook-Pro-de-Caudevilla.local>
2017-07-19 22:06:42 +0800

    finish the testResults functions

--- front-end/AdvanceFrontEnd-DevProjects/ticTacToe/js/javascript.js
+++ front-end/AdvanceFrontEnd-DevProjects/ticTacToe/js/javascript.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 
   $('.popup').css('display','block');
 
-
+// choose your character
   $('#X').click(function() {
     user = $(this).html();
     computer = 'O';
@@ -19,69 +19,104 @@ $(document).ready(function(){
     $('.popup').css('display','none');
   });
 
+// check results
+
+function diagonalTest(){
+  if ((cases[0] == cases[4] && cases[4] == cases[8]) || (cases[2] == cases[4] && cases[4] == cases[6])) {
+    return true;
+  }
+};
+
+function rowTest(){
+  if ((cases[0] == cases[1] && cases[1] == cases[2]) || (cases[3] == cases[4] && cases[4] == cases[5]) || (cases[6] == cases[7] && cases[7] == cases[8])) {
+    return true;
+  }
+};
+
+function columnTest(){
+  if ((cases[0] == cases[3] && cases[3] == cases[6]) || (cases[1] == cases[4] && cases[4] == cases[7]) || (cases[2] == cases[5] && cases[5] == cases [8])) {
+    return true;
+  }
+}
+
+
+// may the game begin
   $('#One').click(function() {
-    var one = $('#One').html();
-    if (one == "") {
-      console.log('success');
+    if (cases[0] == "") {
       $(this).html(user);
+      cases[0] = $(this).html();
+      if (diagonalTest() == true || rowTest() == true || columnTest() == true) {
+        console.log("you won");
+      }
     }
   });
   $('#Two').click(function() {
-    var two = $('#Two').html();
-    if (two == "") {
-      console.log('success');
-      $(this).html(user);
+    if (cases[1] == "") {
+            $(this).html(user);
+      cases[1] = $(this).html();
     }
   });
   $('#Three').click(function() {
-    var three = $('#Three').html();
-    if (three == "") {
-      console.log('success');
-      $(this).html(user);
+    if (cases[2] == "") {
+            $(this).html(user);
+      cases[2] = $(this).html();
+      if (diagonalTest() == true || rowTest() == true || columnTest() == true) {
+        console.log("you won");
+      }
     }
   });
   $('#Four').click(function() {Five
-    var four = $('#Four').html();
-    if (four == "") {
-      console.log('success');
-      $(this).html(user);
+    if (cases[3] == "") {
+            $(this).html(user);
+      cases[3] = $(this).html();
+      if (diagonalTest() == true || rowTest() == true || columnTest() == true) {
+        console.log("you won");
+      }
     }
   });
   $('#Five').click(function() {
-    var five = $('#Five').html();
-    if (five == "") {
-      console.log('success');
-      $(this).html(user);
+    if (cases[4] == "") {
+            $(this).html(user);
+      cases[4] = $(this).html();
+      if (diagonalTest() == true || rowTest() == true || columnTest() == true) {
+        console.log("you won");
+      }
     }
   });
   $('#Six').click(function() {
-    var six = $('#Six').html();
-    if (six == "") {
-      console.log('success');
+    if (cases[5] == "") {
       $(this).html(user);
+      cases[5] = $(this).html();
+      if (diagonalTest() == true || rowTest() == true || columnTest() == true) {
+        console.log("you won");
+      }
     }
   });
   $('#Seven').click(function() {
-    var seven = $('#Seven').html();
-    if (seven == "") {
-      console.log('success');
+    if (cases[6] == "") {
       $(this).html(user);
+      cases[6] = $(this).html();
+      if (diagonalTest() == true || rowTest() == true || columnTest() == true) {
+        console.log("you won");
+      }
     }
   });
   $('#Eight').click(function() {
-    var eight = $('#Eight').html();
-    if (eight == "") {
-      console.log('success');
+    if (cases[7] == "") {
       $(this).html(user);
+      cases[7] = $(this).html();
+      if (diagonalTest() == true || rowTest() == true || columnTest() == true) {
+        console.log("you won");
+      }
     }
   });
   $('#Nine').click(function() {
-    var nine = $('#Nine').html();
-    if (nine == "") {
-      console.log('success');
+    if (cases[8] == '') {
       $(this).html(user);
+      cases[8] = $(this).html();
+      if (diagonalTest() == true || rowTest() == true || columnTest() == true) {
+        console.log("you won");
+      }
     }
   });
-  var one = $('#three').html();
-  console.log(one);
 });
