548179fc2bf51465f1349c4713b16f64edf97b65
Caudevilla Louis <caudevilla@MacBook-Pro-de-Caudevilla.local>
2017-07-19 20:11:10 +0800

    adding twitter and clickable text

--- front-end/AdvanceFrontEnd-DevProjects/ticTacToe/js/javascript.js
+++ front-end/AdvanceFrontEnd-DevProjects/ticTacToe/js/javascript.js
@@ -1,7 +1,20 @@
 $(document).ready(function(){
+
+  var value;
+  var user;
+  var computer;
+
   $('.popup').css('display','block');
 
-  $('.close').click(function(){
+
+  $('#X').click(function() {
+    value = $(this).html();
     $('.popup').css('display','none');
+    console.log(value);
+  });
+  $('#O').click(function() {
+    value = $(this).html();
+    $('.popup').css('display','none');
+    console.log(value);
   });
 });
