01c563141e7f30343b40364ef7da2973c437dccf
Louis Caudevilla <louis.caudevilla@ffshmbp-0054.local>
2017-07-20 17:21:03 +0800

    finish rules and computer moves, now popUp Design

--- front-end/AdvanceFrontEnd-DevProjects/ticTacToe/js/javascript.js
+++ front-end/AdvanceFrontEnd-DevProjects/ticTacToe/js/javascript.js
@@ -23,21 +23,27 @@ $(document).ready(function(){
 
 // check results
 
-function diagonalTest(){
+function diagonalTest(who){
   if ((cases[0] == cases[4] && cases[4] == cases[8]) || (cases[2] == cases[4] && cases[4] == cases[6])) {
     return true;
   }
 };
 
-function rowTest(){
-  if ((cases[0] == cases[1] && cases[1] == cases[2]) || (cases[3] == cases[4] && cases[4] == cases[5]) || (cases[6] == cases[7] && cases[7] == cases[8])) {
+function rowTest(who){
+  if ((cases[0] == who && cases[0] == cases[1] && cases[1] == cases[2]) || (cases[3] == who && cases[3] == cases[4] && cases[4] == cases[5]) || (cases[6] == who && cases[6] == cases[7] && cases[7] == cases[8])) {
     return true;
   }
 };
 
-function columnTest(){
-  if ((cases[0] == cases[3] && cases[3] == cases[6]) || (cases[1] == cases[4] && cases[4] == cases[7]) || (cases[2] == cases[5] && cases[5] == cases [8])) {
+function columnTest(who){
+  if ((cases[0] == who && cases[0] == cases[3] && cases[3] == cases[6]) || (cases[1] == who && cases[1] == cases[4] && cases[4] == cases[7]) || (cases[2] == who && cases[2] == cases[5] && cases[5] == cases [8])) {
     return true;
+  }
+}
+
+function draw(){
+  if (diagonalTest(user) != true && rowTest(user) != true && columnTest(user) != true && userToken.length == 5) {
+    console.log('draw');
   }
 }
 
@@ -46,18 +52,242 @@ function columnTest(){
 function computerMove(){
 
   // first move
-
-  if (user.length == 1) {
+  if (userToken.length == 1) {
     if (userToken[0] == 4) {
         $('#Seven').html(computer);
         cases[6] = $('#Seven').html();
         computerToken.push(6);
     }
-
-    else if (userToken[0] == 0 || userToken[0] == 2 || userToken[0] == 6 || userToken == 8) {
+    else if (userToken[0] == 0 || userToken[0] == 2 || userToken[0] == 6 || userToken == 8 || userToken == 1 || userToken == 3 || userToken == 5 || userToken == 7) {
       $('#Five').html(computer);
       cases[4] = $('#Five').html();
       computerToken.push(4);
+    }
+  }
+
+  // others
+  else if (cases[4] == user && userToken.length != 1) {
+    if (cases[0] == user && cases[8] == '') {
+      $('#Nine').html(computer);
+      cases[8] = $('#Nine').html(computer);
+      computerToken.push(8);
+      if (diagonalTest(computer) == true || rowTest(computer) == true || columnTest(computer) == true) {
+        console.log("computer won");
+      }
+    }
+    else if ((cases[2] == user  && cases[0] == '') || (cases[8] == user && cases[0] == '')) {
+      $('#One').html(computer);
+      cases[0] = computer;
+      computerToken.push(0);
+      if (diagonalTest(computer) == true || rowTest(computer) == true || columnTest(computer) == true) {
+        console.log("computer won");
+      }
+    }
+    else if (cases[1] == user && cases[7] == '') {
+      $('#Eight').html(computer);
+      cases[7] = computer;
+      computerToken.push(7);
+      if (diagonalTest(computer) == true || rowTest(computer) == true || columnTest(computer) == true) {
+        console.log("computer won");
+      }
+    }
+    else if (cases[3] == user && cases[5] == '') {
+      $('#Six').html(computer);
+      cases[5] = computer;
+      computerToken.push(5);
+      if (diagonalTest(computer) == true || rowTest(computer) == true || columnTest(computer) == true) {
+        console.log("computer won");
+      }
+    }
+    else if (cases[5] == user && cases[3] == '') {
+      $('#Four').html(computer);
+      cases[3] = computer;
+      computerToken.push(3);
+      if (diagonalTest(computer) == true || rowTest(computer) == true || columnTest(computer) == true) {
+        console.log("computer won");
+      }
+    }
+    else if (cases[7] == user && cases[1] == '') {
+      $('#Two').html(computer);
+      cases[1] = computer;
+      computerToken.push(1);
+      if (diagonalTest(computer) == true || rowTest(computer) == true || columnTest(computer) == true) {
+        console.log("computer won");
+      }
+    }
+  }
+
+  else if (cases[4] == computer && userToken.length != 1) {
+    console.log('Iam in');
+    if (cases[0] == user && cases[1] == user && cases[2] == '') {
+      $('#Three').html(computer);
+      cases[2] = computer;
+      computerToken.push(2);
+      if (diagonalTest(computer) == true || rowTest(computer) == true || columnTest(computer) == true) {
+        console.log("computer won");
+      }
+    }
+    else if (cases[0] == user && cases[3] == user && cases[6] == '') {
+      $('#Seven').html(computer);
+      cases[6] = computer;
+      computerToken.push(6);
+      if (diagonalTest(computer) == true || rowTest(computer) == true || columnTest(computer) == true) {
+        console.log("computer won");
+      }
+    }
+    else if (cases[0] == user && cases[2] == user && cases[1] == '') {
+      $('#Two').html(computer);
+      cases[1] = computer;
+      computerToken.push(1);
+      if (diagonalTest(computer) == true || rowTest(computer) == true || columnTest(computer) == true) {
+        console.log("computer won");
+      }
+    }
+    else if (cases[1] == user && cases[2] == user && cases[0] == '') {
+      $('#One').html(computer);
+      cases[0] = computer;
+      computerToken.push(0);
+      if (diagonalTest(computer) == true || rowTest(computer) == true || columnTest(computer) == true) {
+        console.log("computer won");
+      }
+    }
+    else if (cases[0] == user && cases[6] == user && cases[3] == '') {
+      $('#Four').html(computer);
+      cases[3] = computer;
+      computerToken.push(3);
+      if (diagonalTest(computer) == true || rowTest(computer) == true || columnTest(computer) == true) {
+        console.log("computer won");
+      }
+    }
+    else if (cases[3] == user && cases[6] == user && cases[0] == '') {
+      $('#One').html(computer);
+      cases[0] = computer;
+      computerToken.push(0);
+      if (diagonalTest(computer) == true || rowTest(computer) == true || columnTest(computer) == true) {
+        console.log("computer won");
+      }
+    }
+    else if (cases[8] == user && cases[2] == user && cases[5] == '') {
+      $('#Six').html(computer);
+      cases[5] = computer;
+      computerToken.push(5);
+      if (diagonalTest(computer) == true || rowTest(computer) == true || columnTest(computer) == true) {
+        console.log("computer won");
+      }
+    }
+    else if (cases[8] == user && cases[5] == user && cases[2] == '') {
+      $('#Three').html(computer);
+      cases[2] = computer;
+      computerToken.push(2);
+      if (diagonalTest(computer) == true || rowTest(computer) == true || columnTest(computer) == true) {
+        console.log("computer won");
+      }
+    }
+    else if (cases[2] == user && cases[5] == user && cases[8] == '') {
+      $('#Nine').html(computer);
+      cases[8] = computer;
+      computerToken.push(8);
+      if (diagonalTest(computer) == true || rowTest(computer) == true || columnTest(computer) == true) {
+        console.log("computer won");
+      }
+    }
+    else if (cases[8] == user && cases[7] == user && cases[6] == '') {
+      $('#Seven').html(computer);
+      cases[6] = computer;
+      computerToken.push(6);
+      if (diagonalTest(computer) == true || rowTest(computer) == true || columnTest(computer) == true) {
+        console.log("computer won");
+      }
+    }
+    else if (cases[6] == user && cases[7] == user && cases[8] == '') {
+      $('#Nine').html(computer);
+      cases[8] = computer;
+      computerToken.push(8);
+      if (diagonalTest(computer) == true || rowTest(computer) == true || columnTest(computer) == true) {
+        console.log("computer won");
+      }
+    }
+    else if (cases[8] == user && cases[6] == user && cases[7] == '') {
+      $('#Eight').html(computer);
+      cases[7] = computer;
+      computerToken.push(7);
+      if (diagonalTest(computer) == true || rowTest(computer) == true || columnTest(computer) == true) {
+        console.log("computer won");
+      }
+    }
+    else {
+      if (cases[0] == '') {
+        $('#One').html(computer);
+        cases[0] = computer;
+        computerToken.push(0);
+        if (diagonalTest(computer) == true || rowTest(computer) == true || columnTest(computer) == true) {
+          console.log("computer won");
+        }
+      }
+      else if (cases[1] == '') {
+        $('#Two').html(computer);
+        cases[1] = computer;
+        computerToken.push(1);
+        if (diagonalTest(computer) == true || rowTest(computer) == true || columnTest(computer) == true) {
+          console.log("computer won");
+        }
+      }
+      else if (cases[2] == '') {
+        $('#Three').html(computer);
+        cases[2] = computer;
+        computerToken.push(2);
+        if (diagonalTest(computer) == true || rowTest(computer) == true || columnTest(computer) == true) {
+          console.log("computer won");
+        }
+      }
+      else if (cases[3] == '') {
+        $('#Four').html(computer);
+        cases[3] = computer;
+        computerToken.push(3);
+        if (diagonalTest(computer) == true || rowTest(computer) == true || columnTest(computer) == true) {
+          console.log("computer won");
+        }
+      }
+      else if (cases[4] == '') {
+        $('#Five').html(computer);
+        cases[4] = computer;
+        computerToken.push(4);
+        if (diagonalTest(computer) == true || rowTest(computer) == true || columnTest(computer) == true) {
+          console.log("computer won");
+        }
+      }
+      else if (cases[5] == '') {
+        $('#Six').html(computer);
+        cases[5] = computer;
+        computerToken.push(5);
+        if (diagonalTest(computer) == true || rowTest(computer) == true || columnTest(computer) == true) {
+          console.log("computer won");
+        }
+      }
+      else if (cases[6] == '') {
+        $('#Seven').html(computer);
+        cases[6] = computer;
+        computerToken.push(6);
+        if (diagonalTest(computer) == true || rowTest(computer) == true || columnTest(computer) == true) {
+          console.log("computer won");
+        }
+      }
+      else if (cases[7] == '') {
+        $('#Eight').html(computer);
+        cases[7] = computer;
+        computerToken.push(7);
+        if (diagonalTest(computer) == true || rowTest(computer) == true || columnTest(computer) == true) {
+          console.log("computer won");
+        }
+      }
+      else if (cases[8] == '') {
+        $('#Nine').html(computer);
+        cases[8] = computer;
+        computerToken.push(8);
+        if (diagonalTest(computer) == true || rowTest(computer) == true || columnTest(computer) == true) {
+          console.log("computer won");
+        }
+      }
     }
   }
 
@@ -74,7 +304,7 @@ function computerMove(){
       computerMove();
 
       console.log(userToken);
-      if (diagonalTest() == true || rowTest() == true || columnTest() == true) {
+      if (diagonalTest(user) == true || rowTest(user) == true || columnTest(user) == true) {
         console.log("you won");
       }
     }
@@ -88,9 +318,11 @@ function computerMove(){
 
       computerMove();
 
-      if (diagonalTest() == true || rowTest() == true || columnTest() == true) {
+      if (diagonalTest(user) == true || rowTest(user) == true || columnTest(user) == true) {
         console.log("you won");
       }
+      draw();
+
     }
   });
 
@@ -102,9 +334,11 @@ function computerMove(){
 
       computerMove();
 
-      if (diagonalTest() == true || rowTest() == true || columnTest() == true) {
+      if (diagonalTest(user) == true || rowTest(user) == true || columnTest(user) == true) {
         console.log("you won");
       }
+      draw();
+
     }
   });
 
@@ -116,9 +350,11 @@ function computerMove(){
 
       computerMove();
 
-      if (diagonalTest() == true || rowTest() == true || columnTest() == true) {
+      if (diagonalTest(user) == true || rowTest(user) == true || columnTest(user) == true) {
         console.log("you won");
       }
+      draw();
+
     }
   });
 
@@ -130,9 +366,11 @@ function computerMove(){
 
       computerMove();
 
-      if (diagonalTest() == true || rowTest() == true || columnTest() == true) {
+      if (diagonalTest(user) == true || rowTest(user) == true || columnTest(user) == true) {
         console.log("you won");
       }
+      draw();
+
     }
   });
 
@@ -144,9 +382,11 @@ function computerMove(){
 
       computerMove();
 
-      if (diagonalTest() == true || rowTest() == true || columnTest() == true) {
+      if (diagonalTest(user) == true || rowTest(user) == true || columnTest(user) == true) {
         console.log("you won");
       }
+      draw();
+
     }
   });
 
@@ -158,9 +398,10 @@ function computerMove(){
 
       computerMove();
 
-      if (diagonalTest() == true || rowTest() == true || columnTest() == true) {
+      if (diagonalTest(user) == true || rowTest(user) == true || columnTest(user) == true) {
         console.log("you won");
       }
+      draw();
     }
   });
 
@@ -172,9 +413,11 @@ function computerMove(){
 
       computerMove();
 
-      if (diagonalTest() == true || rowTest() == true || columnTest() == true) {
+      if (diagonalTest(user) == true || rowTest(user) == true || columnTest(user) == true) {
         console.log("you won");
       }
+      draw();
+
     }
   });
 
@@ -186,9 +429,11 @@ function computerMove(){
 
       computerMove();
 
-      if (diagonalTest() == true || rowTest() == true || columnTest() == true) {
+      if (diagonalTest(user) == true || rowTest(user) == true || columnTest(user) == true) {
         console.log("you won");
       }
+      draw();
+
     }
   });
 });
