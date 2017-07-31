190b2c1ade1eceddf20943dc3f831f5d9b16ba17
Louis Caudevilla <louis.caudevilla@ffshmbp-0054.local>
2017-07-31 17:49:46 +0800

    Updating the ID names and creating main object in javascript

--- front-end/AdvanceFrontEnd-DevProjects/simonGame/js/javascript.js
+++ front-end/AdvanceFrontEnd-DevProjects/simonGame/js/javascript.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
-
+  var game = {
+    count: 0,
+    possibilities: ['#green','#red', '#yellow', '#blue'],
+    currentGame: [],
+    player: [],
+    sound:{
+      green: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3'),
+      red: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3'),
+      yellow: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3'),
+      blue: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3')
+    },
+    godlike: false,
+  }
 });
